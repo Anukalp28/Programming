0e4a2a2386d79bb338a1d9aabacdf2055a87bbc5
Anukalp Raj <anukalp2828@gmail.com>
2026-07-22 22:54:24 +0530

    java questions

---
+++ Java programming/sem 3/My_pro/Practice3.java
@@ -0,0 +1,43 @@
+// public class Practice3{
+//     public static void main(String[] args) {
+//         //boolean 
+//         // boolean a = true;
+//         // boolean b = false;
+
+//         // System.out.println(a);
+//         // System.out.println(b);
+        
+//         //byte Data from [-128 to 127]
+//         // byte age = 25;
+//         // byte x = 127;
+
+//         short s = 1000;
+//         int i = 1264;
+//         long l = 35464654L;
+//         float pi = 3.141526f;
+//         double avag = 6.022;
+//         char n = 'A';
+
+//         // String
+//         String naem = "Anukalp";
+
+//     }
+// }
+
+
+// literals
+public class Practice3{
+    public static void main(String[] args) {
+        int a = 20;
+
+        int b = 0125;
+        // int c = 0XFACB;
+        int c = 0xfacb;
+        int bin = 0b11111;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(bin);
+    }
+}
