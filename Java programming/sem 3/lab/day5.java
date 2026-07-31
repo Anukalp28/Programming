eab2a919a36a5c29e42be1667e1ae3039ba8ba5e
Anukalp Raj <anukalp2828@gmail.com>
2026-07-31 22:41:55 +0530

    java questions

--- Java programming/sem 3/lab/day5.java
+++ Java programming/sem 3/lab/day5.java
@@ -48,41 +48,136 @@
 
 
 // Q3 Write a Java Program to Fibonacci Series in Java.
-// Q4 Write a Java Program to Print Pattern in Java.
-// Q5 Write a Java Program to Armstrong Number in Java.
 import java.util.Scanner;
 public class day5{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number:");
-        int n = sc.nextInt();
-        // int n = 153;
-        // int n = 1634;
-        int temp = n;
-        int digit = 0;
-        while(temp!=0){ 
-            digit++;
-            temp /= 10;
-        }
-        System.out.println(digit);
-        temp = n;
-        double sum = 0;
-        while(temp!=0){ 
-            int rem = temp%10;
-            sum = sum + Math.pow(rem, digit);
-            temp /= 10;
-        }
-        System.out.println(sum);
-        if(n==sum){
-            System.out.println("Armstrong Number");
-
-        }
-        else{
-            System.out.println("Not a Armstrong Number");
-        }
-        sc.close();
+        int n,a=0,b=1,c;
+        System.out.print("Enter no of terms:");
+        n = sc.nextInt();
+        if (n>=1) System.out.print(a+" ");
+        if (n>=2) System.out.print(b+" ");
         
+        for (int i=1;i<n;i++){
+            c = a+b;
+            System.out.print(c+" ");
+            a=b;
+            b=c;
+        }
+
     }
 }
+
+
+// Q4 Write a Java Program to Print Pattern in Java.
+//         *         
+//       * * *       
+//     * * * * *     
+//   * * * * * * *   
+// * * * * * * * * *
+// public class day5{
+//     public static void main(String[] args){
+//         int n = 5;
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=n;j++){
+//                 // System.out.printf("(%d %d)",i,j);
+//                 if(i+j>=6){
+//                     // System.out.printf("(%d %d)",i,j);
+//                     System.out.print("* ");
+//                 }
+//                 else{
+//                     // System.out.printf("(   )",i,j);
+//                     System.out.print("  ");
+//                 }
+//             }
+//             for(int j=2;j<=n;j++){
+//                 // System.out.printf("(%d %d)",i,j);
+//                 if(i>=j){
+//                     // System.out.printf("(%d %d)",i,j);
+//                     System.out.print("* ");
+//                 }
+//                 else{
+//                     // System.out.printf("(   )",i,j);
+//                     System.out.print("  ");
+//                 }
+//             }
+//             System.out.println();
+
+//         }
+//     }
+// }
+
+// Q5 Write a Java Program to Armstrong Number in Java.
+// import java.util.Scanner;
+// public class day5{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter a number:");
+//         int n = sc.nextInt();
+//         // int n = 153;
+//         // int n = 1634;
+//         int temp = n;
+//         int digit = 0;
+//         while(temp!=0){ 
+//             digit++;
+//             temp /= 10;
+//         }
+//         System.out.println(digit);
+//         temp = n;
+//         double sum = 0;
+//         while(temp!=0){ 
+//             int rem = temp%10;
+//             sum = sum + Math.pow(rem, digit);
+//             temp /= 10;
+//         }
+//         System.out.println(sum);
+//         if(n==sum){
+//             System.out.println("Armstrong Number");
+
+//         }
+//         else{
+//             System.out.println("Not a Armstrong Number");
+//         }
+//         sc.close();
+        
+//     }
+// }
 // Q6 Write a Java Program to Reverse a Number in Java.
+// import java.util.Scanner;
+// public class day5{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter a two no.s:");
+//         int n1 = sc.nextInt();
+//         // int n1 = 12345;
+//         int rev = 0;
+//         while(n1>0){
+//             int rem = n1%10;
+//             // System.out.println(rem);
+//             rev = rev+rem;
+//             n1 /= 10;
+//             if(n1>0){
+//                 rev = rev*10;
+//                 // System.out.println(rev);
+//             }
+//         }
+//         System.out.print(rev);
+//     }
+// }
+
 // Q7 Write a Java Program to Find Sum of Natural Numbers.
+// import java.util.Scanner;
+// public class day5{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter a number:");
+//         int n = sc.nextInt();
+//         // int n = 10;
+//         int sum = 0;
+//         for(int i=1;i<=n;i++){
+//             sum += i;
+//         }
+//         System.out.println("Sum of Natural Numbers:"+sum);
+//     }
+// }
