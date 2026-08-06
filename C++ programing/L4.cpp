92582a2cd0e7a3757986da71d42df0f6a7fd0f7a
Anukalp Raj <anukalp2828@gmail.com>
2026-08-06 22:13:38 +0530

    C++ practice (Questions)

--- C++ programing/L4.cpp
+++ C++ programing/L4.cpp
@@ -30,17 +30,62 @@
 // Think about how mathematical properties like the Lowest Common Multiple (LCM) can reduce
 // multi-step rules).
 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter a number ";
+//     cin>>n;
+//     if(n%3==0){
+//         if(n%5==0){
+//             cout<<"Divisible by both 3 and 5"<<endl;
+//         }
+//         else{
+//             cout<<"Divisible by both 3 but not by 5"<<endl;
+//         }
+//     }
+//     else cout<<"Not Divisible by both 3 and 5"<<endl;
+// }
+
+// Q13. Given the number of sides n of a simple polygon and the sum of all its interior angles
+// S, write a program to check if the user has provided valid dimensional specifications. Use the
+// structural formula:
+// S = (n − 2) × 180◦
+// Sample — Input: Sides = 3, Sum = 180 ⇒ Output: Valid Polygo
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n,ang,s;
+//     cout<<"Enter sides:";
+//     cin>>n;
+//     cout<<"Enter Angles:";
+//     cin>>ang;
+
+//     s = (n-2)*180;
+//     if(s==180 || s==360) cout<<"Valid polygon"<<endl;
+
+
+// }
+
+
+// Q14. Take two integers as input. If exactly one of them is odd, print "One is odd". If
+// both are odd, print "Both are odd". If neither of them is odd, print "None is odd". Use
+// conditional constructs effectively.
+// Sample — Input: 4 7 ⇒ Output: One is odd
 #include<iostream>
 using namespace std;
 int main(){
-    int n;
-    cout<<"Enter a number ";
-    cin>>n;
-    if(n%3==0){
-        if(n%5==0){
-            cout<<"Divisible by both 3 and 5"<<endl;
-        }
-        cout<<"Divisible by both 3 but not by 5"<<endl;
+    int n1,n2;
+    cout<<"Enter two number:";
+    cin>>n1;
+    cin>>n2;
+    if(n1%2!=0 || n2%2!=0){
+        cout<<"One is odd"<<endl;
     }
-    else cout<<"Not Divisible by both 3 and 5"<<endl;
+    else if(n1%2!=0 && n2%2!=0){
+        cout<<"both are odd"<<endl;
+    }
+    else cout<<"NOne is odd"<<endl;
+
 }
