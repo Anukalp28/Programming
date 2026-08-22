803118980bf7a253bcb5d5975e75d095d46a0f60
Anukalp Raj <anukalp2828@gmail.com>
2026-08-23 00:47:10 +0530

    DS mini-project(A)

--- C programming/sem-3/DS/Assignment1.c
+++ C programming/sem-3/DS/Assignment1.c
@@ -16,11 +16,11 @@
 // void insertAtIndex(int arr[], int *len, int size, int element, int index){
 //     if(*len>=size){
 //         printf("Error: Array Overflow. Cannot insert.\n");
-//         // return;
+//         return;
 //     }
 //     if(index <0||index>*len){
 //         printf("Error: Invalid position.\n");
-//         // return;
+//         return;
 //     }
     
 //     for(int i=*len;i>index;i--){
@@ -83,9 +83,104 @@
 // identification of topper,
 // and generation of a simple report
 #include<stdio.h>
+#include<string.h>
+
+void record_entry(int arr[][4],int strLen){
+    int RolNo,p,c,m;
+    printf("\nEnter Rollno.:");scanf("%d",&RolNo);
+    // for(int i=0;i<strLen;i++){
+    //     if(RolNo==arr[i][0]){
+    //         printf("Rollno already exist\n");
+    //         // return '\0';
+    //     }
+    // }
+    printf("\nEnter Physics marks:");scanf("%d",&p);
+    printf("\nEnter Chemistry marks:");scanf("%d",&c);
+    printf("\nEnter Maths marks:");scanf("%d",&m);
+    arr[strLen][0] = RolNo;
+    arr[strLen][1] = p;
+    arr[strLen][2] = c;
+    arr[strLen][3] = m;
+    
+}
+void display(int arr[][4],int len){
+    printf("Rollno\tPhysics\tChemistry\tMaths\n");
+    for(int i=0;i<len;i++){
+        printf("%d \t%d\t%d\t%d\n",arr[i][0],arr[i][1],arr[i][2],arr[i][3]);
+    }
+}
+
+void total_avg_marks(int arr[][4],int len){
+    printf("Rollno\tTotal\tavg\n");
+    int total;
+    float avg;
+    for(int i=0;i<len;i++){
+        total = arr[i][1]+arr[i][2]+arr[i][3];
+        avg = total/3.0;
+        printf("%d \t%d \t%f\n",arr[i][0],total,avg);
+    }
+}
+
+int topper(int arr[][4],int len){
+    int avg;
+    int topRoll = 0;
+    float topAvgMarks = 0;
+    for(int i=0;i<len;i++){
+        avg = (arr[i][1]+arr[i][2]+arr[i][3])/3.0;
+        if(topAvgMarks<avg){
+            topAvgMarks = avg;
+            topRoll = arr[i][0];
+        }
+    }
+    printf("Toper Rollno.:%d and avg marks:%f",topRoll,topAvgMarks);
+}
+
+void report(int arr[][4],int len){
+    int RolNo,Rindx;
+    float avg;
+    printf("\nEnter Rollno.:");scanf("%d",&RolNo);
+    for(int i=0;i<len;i++){
+        if(RolNo==arr[i][0]){
+            Rindx = i;
+            break;
+        }
+    }
+    printf("Subject \tMarks\n");
+    printf("Physics   \t%d\n",arr[Rindx][1]);
+    printf("Chemistry \t%d\n",arr[Rindx][2]);
+    printf("Maths     \t%d\n",arr[Rindx][3]);
+
+    avg = (arr[Rindx][1]+arr[Rindx][2]+arr[Rindx][3])/3.0;
+    printf("Average marks:%f",avg);
+
+}
+
 int main(){
-    // {rolno,name,p,c,m}
-    int stRecord[100][5] = {10,20,30,40,50};
-    int size = sizeof(stRecord)/sizeof(stRecord[1][0]);
-    printf("%d ",size);
+    // {rolno,name,p,c,m}  //{rolno,p,c,m}
+    int stRecord[100][4] = {10,30,40,50,
+                            11,50,50,50,
+                            12,60,60,60,
+                            13,65,75,80,
+                            14,80,85,90
+    };
+    int stLen=0;
+    stLen++;
+    stLen++;
+    stLen++;
+    stLen++;
+    stLen++;
+
+    
+    int size = sizeof(stRecord)/sizeof(stRecord[0]);
+    // printf("%d ",size);
+    display(stRecord,stLen);
+    // record_entry(stRecord,stLen);stLen++;
+    // record_entry(stRecord,stLen);stLen++;
+    // display(stRecord,stLen);
+    total_avg_marks(stRecord,stLen);
+    topper(stRecord,stLen);
+    report(stRecord,stLen);
+    report(stRecord,stLen);
+
+
 }
